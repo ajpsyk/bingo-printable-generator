e71ec115f7c4a8844a97115f69255c5dd35a361f
ajpsyk <ajpsyk@gmail.com>
2025-09-03 19:31:48 -0700

    restructured documentconfig to a builder pattern

--- src/main/java/org/bingo/model/DocumentConfig.java
+++ src/main/java/org/bingo/model/DocumentConfig.java
@@ -1,28 +1,28 @@
 package org.bingo.model;
 
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.geom.PageSize;
+
+import java.util.Random;
+
 import lombok.Data;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NonNull;
 
-import java.nio.file.Path;
-
+/**
+ * Holds document level configurations.
+ */
 @Data
-@AllArgsConstructor
+@Builder
 public class DocumentConfig {
-    private String themeName;
+    @NonNull AssetPaths assets;
 
-    private Path icons;
-    private Path header;
-    private Path frame;
-    private Path freeSpace;
-    private Path callingCardsHeader;
-    private Path token;
-    private Path instructions;
-    private Path font;
-    private Path scissorsIcon;
-    private Path output;
+    @Builder.Default DeviceRgb fontColor = new DeviceRgb(0,0,0);
+    @Builder.Default PageSize pageSize = PageSize.LETTER;
+    @Builder.Default Random seed = new Random(1);
 
-    private float marginTopInches;
-    private float marginRightInches;
-    private float marginBottomInches;
-    private float marginLeftInches;
+    @Builder.Default float marginTopInches = 0f;
+    @Builder.Default float marginRightInches = 0f;
+    @Builder.Default float marginBottomInches = 0f;
+    @Builder.Default float marginLeftInches = 0f;
 }
